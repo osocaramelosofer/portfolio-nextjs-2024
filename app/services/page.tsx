54ba97954db6d5eee56e065e16cd599ebe518a03
Fernando Miguel <miguel@itnora.com>
2023-11-13 10:40:24 -0600

    WIP lottie

---
+++ app/services/page.tsx
@@ -0,0 +1,14 @@
+export default function ServicesPage () {
+  return (
+    <main>
+      <div className="flex flex-col h-screen items-center justify-center bg-amber-200 gap-10">
+        <h1 className="text-4xl pageHeader">About Page</h1>
+        <p className="mx-10 pageContent text-center line-clamp-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac urna
+          auctor, viverra sapien. Donec euismod turpis eget massa lobortis, eget
+          scelerisque justo.
+        </p>
+      </div>
+    </main>
+  )
+}
