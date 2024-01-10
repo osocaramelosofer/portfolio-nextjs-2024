41be7d45c538f741726592e6c39043c9205818ab
Fernando Miguel <miguel@itnora.com>
2024-01-10 12:57:48 -0600

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home () {
   <section className='hero h-auto py-20 relative'>
     <div className='container mx-auto px-12  w-full'>
       <div className='hero-content grid gap-y-4'>
-        <div className='hero-main bg-yellow-400 flex flex-col-reverse  md:justify-center items-center md:flex-row'>
-            <div className='hero-text flex flex-col gap-2 max-w-[800px] bg-red-500 relative'>
+        <div className='hero-main flex flex-col-reverse  md:justify-center items-center md:flex-row'>
+            <div className='hero-text flex flex-col gap-2 max-w-[800px] relative'>
               <div className='flex w-full relative'>
                 <h1 className=
                 {`${outfit.className} ${styles.title}
@@ -23,8 +23,34 @@ export default function Home () {
               </div>
               <p>Hi, I'm Fernando Hernandez. A passionate Front-end Developer based in Tlaxcala, Mexico. 📍</p>
               <span className='flex gap-3'>
-                <a href="" className='cursor-pointer'>github</a>
-                <a href="" className='cursor-pointer'>linkedin</a>
+                <a href="https://github.com/osocaramelosofer" className='cursor-pointer'>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="tabler-icon tabler-icon-brand-github"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+                  </svg>
+                </a>
+                <a href="https://www.linkedin.com/in/fernando-hernández-8b30421b2/" className='cursor-pointer'>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="tabler-icon tabler-icon-brand-linkedin"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M8 11l0 5"></path><path d="M8 8l0 .01"></path><path d="M12 16l0 -5"></path><path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+                </svg>
+                </a>
               </span>
             </div>
             <figure className='hero-img rounded-full overflow-hidden max-w-[200px] max-h-[200px]'>
--- components/dots.tsx
+++ components/dots.tsx
@@ -1,14 +1,17 @@
 export function Dots () {
   return (
-        <>
-            <div
-            className="bg-[#F5E3FD] absolute top-[0rem] -z-10 right-[0rem] h-[10.25rem]
-            w-[10.25rem] rounded-full blur-[2rem] dark:blur-[6rem]  sm:w-[12.75rem] sm:h-[12.75rem] dark:bg-[#F5E3FD]"
-            />
-            <div
-            className="bg-[#CBD7FF] absolute top-[30rem] -z-10 left-[0rem] h-[12.25rem]
-            w-[12.25rem] rounded-full blur-[3rem] dark:blur-[7rem]  sm:w-[16.75rem] sm:h-[16.75rem] dark:bg-[#CBD7FF]"
-            ></div>
-        </>
+    <>
+      <div
+      className="bg-[#F5E3FD] absolute top-[0rem] -z-10 right-[0rem] h-[10.25rem]
+      w-[10.25rem] rounded-full blur-[2rem] dark:blur-[6rem]  sm:w-[12.75rem] sm:h-[12.75rem] dark:bg-[#F5E3FD]"
+      />
+      <div
+      className="bg-[#e3fdf6] absolute top-[0rem] -z-10 left-[0rem] h-[8.25rem]
+      w-[8.25rem] rounded-full blur-[2rem] dark:blur-[6rem]  sm:w-[10.75rem] sm:h-[10.75rem] dark:bg-[#F5E3FD]"
+      />
+      <div className="bg-[#CBD7FF] absolute top-[30rem] -z-10 left-[0rem] h-[12.25rem]
+      w-[12.25rem] rounded-full blur-[3rem] dark:blur-[7rem]  sm:w-[16.75rem] sm:h-[16.75rem] dark:bg-[#CBD7FF]"
+      />
+    </>
   )
 }
