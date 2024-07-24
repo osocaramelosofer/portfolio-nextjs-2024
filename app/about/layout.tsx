9acc9036a35553b506777a9e3fa5cf858187915e
Austin <babelontrip@gmail.com>
2024-07-23 19:16:12 -0600

    WIP authentication

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,4 +1,6 @@
 import { createClient } from "@/lib/supabase/server"
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function AboutLayout({children}:{children: React.ReactNode}) {
     const supabase = createClient()
@@ -7,6 +9,19 @@ export default async function AboutLayout({children}:{children: React.ReactNode}
     // console.log("notes:", notes)
     console.log("users:", users)
     // console.log("keys:", process.env.NEXT_PUBLIC_SUPABASE_URL)
+    const origin = headers().get('origin')
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+          redirectTo: `${origin}`,
+        },
+      })
+      
+      if (data.url) {
+        redirect(data.url) // use the redirect API for your server framework
+      }
+      
+      
     return(
         <>
         <div>
