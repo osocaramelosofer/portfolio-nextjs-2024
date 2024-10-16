c634025af12f32ec6b14c7b8eee7584627748053
Austin <babelontrip@gmail.com>
2024-10-15 19:48:43 -0600

    wip new design

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
-import { AboutSection } from '@/components/about/about-section'
-import HeroSection from '@/components/hero/hero-section'
-import Projects from '@/components/projects/projects'
-
 export default function Home () {
   return (
-  <>
-    <HeroSection />
-    <AboutSection/>
-    <Projects/>
-  </>
+  <main>
+    <section>
+      <div className="container">
+        <div className="hero">
+          <div>
+            <span>about me</span>
+          </div>
+          <h2>Fernando Hernandez</h2>
+          <p>Fullstack developer with +5 years of experience building web applications</p>
+        </div>
+        <div className="2">
+          <div>
+            <span>theme</span>
+          </div>
+          <div>
+            <span>weather</span>
+            <span>45</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
   )
 }
