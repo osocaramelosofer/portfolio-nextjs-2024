80da0d0c51c82499112f73d20e96ed2ed0e24d90
Austin <babelontrip@gmail.com>
2024-12-10 11:51:00 -0600

    WIP react page content

--- app/react/page.tsx
+++ app/react/page.tsx
@@ -1,14 +1,13 @@
 export default function ReactPage() {
   return (
-    <section>
-      <h1 className="text-center text-2xl font-semibold">React Exercises 📄</h1>
+    <div className="px-2">
+      <h1 className="text-2xl font-semibold">React Exercises </h1>
       <article className="text-lg">
         <p>
-          These are ⚛️ react helpful exercises I have found in my journey as a
+          These are react helpful exercises I have found in my journey as a
           frontend developer.
         </p>
-        <p>You can use to go over them to get ready for technical interviews</p>
       </article>
-    </section>
+    </div>
   )
 }
