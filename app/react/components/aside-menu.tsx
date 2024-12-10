e291a3551738581e08177a952d2ef4fa748fd92c
Austin <babelontrip@gmail.com>
2024-12-10 09:48:38 -0600

    fix aside-menu children items

--- app/react/components/aside-menu.tsx
+++ app/react/components/aside-menu.tsx
@@ -18,17 +18,14 @@ export default function AsideMenu() {
     <aside className="mr-5 bg-foreground-50">
       <ul className="flex flex-col px-2">
         {EXERCISES.map(({ key, name }, idx) => {
-          const isActive = useActivePath(key)
+          const isActive = useActivePath({ currentPage: key })
           return (
-            <>
-              {idx > 0 && <hr className="border-gray-400 " />}
-              <li
-                key={key}
-                className={`${isActive ? 'text-sky-400' : ''} text-lg`}
-              >
-                <Link href={`/react/guides/${key}`}>{name}</Link>
-              </li>
-            </>
+            <li
+              key={key}
+              className={`${isActive ? 'text-sky-400' : ''} text-lg`}
+            >
+              <Link href={`/react/guides/${key}`}>{name}</Link>
+            </li>
           )
         })}
       </ul>
