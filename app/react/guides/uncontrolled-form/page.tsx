6fe97c82146b7ba29d74a97ff98f1cbe4a58a7b3
Austin <babelontrip@gmail.com>
2024-12-11 11:39:28 -0600

    update react guides

--- app/react/guides/uncontrolled-form/page.tsx
+++ app/react/guides/uncontrolled-form/page.tsx
@@ -1,44 +1,64 @@
 'use client'
-import {
-  SandpackProvider,
-  SandpackLayout,
-  SandpackCodeEditor,
-  SandpackPreview,
-  SandpackFileExplorer,
-  type SandpackFile
-} from '@codesandbox/sandpack-react'
+
+import { type SandpackFiles } from '@codesandbox/sandpack-react'
+import CustomSandpack from '../../custom-sandpack'
+import { TextScrambleBasic } from '@/components/motion-ui/text-scramble-basic'
 export default function UncontrolledFormPage() {
-  const files: SandpackFile[] = [
-    {
-      code: 'hola mundo',
-      hidden: false,
-      active: true,
-      readOnly: true
-    },
-    {
-      code: 'adios mundo',
+  const files: SandpackFiles = {
+    '/App.js': {
+      code: `export default function MyForm() {
+  function handleSubmit(e) {
+    // Previene que el navegador recargue la página
+    e.preventDefault();
+
+    // Lee los datos del formulario
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // Puedes pasar formData como el cuerpo de la consulta directamente:
+    fetch('/some-api', { method: form.method, body: formData });
+
+    // O puedes trabajar con él como un objecto plano:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <label>
+        Input de texto: <input name="myInput" defaultValue="Some initial value" />
+      </label>
+      <hr />
+      <label>
+        Checkbox: <input type="checkbox" name="myCheckbox" defaultChecked={true} />
+      </label>
+      <hr />
+      <p>
+        Botones radio:
+        <label><input type="radio" name="myRadio" value="option1" /> Opción 1</label>
+        <label><input type="radio" name="myRadio" value="option2" defaultChecked={true} /> Opción 2</label>
+        <label><input type="radio" name="myRadio" value="option3" /> Opción 3</label>
+      </p>
+      <hr />
+      <button type="reset">Reiniciar formulario</button>
+      <button type="submit">Enviar formulario</button>
+    </form>
+  );
+}
+`,
       hidden: false,
       active: true,
       readOnly: true
     }
-  ]
+  }
+
   return (
     <div>
-      <h1>Uncontrolled Form</h1>
-      <SandpackProvider
-        template="react"
-        theme="dark"
-        files={{
-          '/app': files[0],
-          '/test': files[1]
-        }}
-      >
-        <SandpackLayout>
-          <SandpackFileExplorer />
-          <SandpackCodeEditor />
-          <SandpackPreview />
-        </SandpackLayout>
-      </SandpackProvider>
+      <TextScrambleBasic className="text-3xl">
+        Uncontrolled Form
+      </TextScrambleBasic>
+
+      <CustomSandpack files={files} />
     </div>
   )
 }
