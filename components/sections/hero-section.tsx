b35938f54db0741dd57498d505c6750497970422
Austin <babelontrip@gmail.com>
2024-12-13 13:13:53 -0600

    Add border trail to hero component

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,3 +1,4 @@
+'use client'
 import ThemeToggle from '@/components/ui/theme-toogle'
 import { Button } from '@nextui-org/react'
 import { FaGithub, FaLinkedin, FaUser } from 'react-icons/fa'
@@ -9,11 +10,45 @@ import { WeatherComponent } from '../weather-component'
 import './hero.css'
 
 import { TextScramble } from '../motion-primitives/core/text-scramble'
+import { BorderTrail } from '../motion-primitives/core/border-trail'
+import { useState } from 'react'
+import { cn } from '@/lib/utils'
 export function HeroSection() {
+  const [isVisible, setIsVisible] = useState(true)
+
+  const handleAnimationComplete = () => {
+    setIsVisible(false)
+  }
+  const handleMouseEnter = () => {
+    setIsVisible(true)
+  }
+  const handleMouseLeave = () => {
+    setIsVisible(false)
+  }
+
   return (
     <section className="mb-3">
       <div className="hero-grid">
-        <div className="grid-area-about hero flex flex-col justify-between rounded-lg bg-foreground-100 p-4">
+        <div
+          className="grid-area-about hero group relative flex flex-col justify-between rounded-lg bg-foreground-100 p-4"
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+        >
+          {isVisible && (
+            <BorderTrail
+              transition={{
+                ease: [0, 0.5, 0.8, 0.5],
+                duration: 4,
+                repeat: 1
+              }}
+              onAnimationComplete={handleAnimationComplete}
+              className={cn(
+                'bg-gradient-to-l from-sky-300 via-sky-500 to-sky-300 transition-opacity duration-300 dark:from-orange-700 dark:via-orange-500 dark:to-orange-700',
+                'opacity-0 group-hover:opacity-100'
+              )}
+              size={150}
+            />
+          )}
           <div className="flex items-center gap-x-2">
             <FaUser className="rounded-lg bg-black p-2 text-4xl text-white " />
             <span className="font-rubik">about me</span>
@@ -75,7 +110,8 @@ export function HeroSection() {
             </div>
           </div>
         </div>
-        <div className="grid-area-location flex flex-col gap-y-10 rounded-lg bg-foreground-100 p-4 ">
+        <div className="grid-area-location flex flex-col gap-y-10 rounded-lg bg-foreground-100 p-4 hover:relative ">
+          {/* <BorderTrail size={120} /> */}
           <div className="flex items-center gap-x-2 ">
             <IoIosColorPalette className="rounded-lg bg-black p-2 text-4xl text-white" />
             <span className="font-lexend text-lg font-semibold capitalize">
